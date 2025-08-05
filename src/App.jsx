7f1c3da2e8937af4bb90d1496b22c38d0e9fe752
greatby <gills_halogen2i@icloud.com>
2025-08-05 15:45:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WhatsAppRedirect from './components/whatsappDirect'
+
+const App = () => {
+  return (
+   <>
+   <WhatsAppRedirect />
+   </>
+  )
+}
+
+export default App
